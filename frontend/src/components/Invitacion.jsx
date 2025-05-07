c6cf17a16bfde1bc57545de0dcd2cbf414ab14c4
AngelCode2001 <angel.deleon@code.com.gt>
2025-05-07 08:42:02 -0600

    cambio invitacion css

--- frontend/src/components/Invitacion.jsx
+++ frontend/src/components/Invitacion.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "../styles/invitacion.css";
+import "../styles/Invitacion.css";
 import Osos from "../assets/image.png";
 import IMGLugar from "../assets/lugar.png";
 
